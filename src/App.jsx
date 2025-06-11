41ced1489c7e386e5094abfc9484e9fec1ee68b5
kumar kalyan <kumarkalyan200320@gmail.com>
2025-06-11 23:55:23 +0530

    added a code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
+import React from 'react';
 import './App.css';
-import Header from './components/Header';
+import Header from './components/header/Header';
 import Home from './components/home/Home';
 
 const App = () => {
